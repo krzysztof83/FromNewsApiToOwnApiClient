4b1cd80c95e70885b4e1f501cdcaeaf59f611815
Krzychu83 <k.czechowski83@gmail.com>
2018-12-31 19:10:57 +0100

    init project

---
+++ src/main/java/com/czechowski/fromnewsapitoownapiclient/FromnewsapitoownapiclientApplication.java
@@ -0,0 +1,14 @@
+package com.czechowski.fromnewsapitoownapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromnewsapitoownapiclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromnewsapitoownapiclientApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/czechowski/fromnewsapitoownapiclient/FromnewsapitoownapiclientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.czechowski.fromnewsapitoownapiclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FromnewsapitoownapiclientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
